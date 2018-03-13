ac2f956c12173e8c04321462e2298dd43c841abe
Josue Sanchez <josueguitar2@gmail.com>
2018-03-12 23:40:15 -0400

    LC

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,12 +1,13 @@
 import { AppModule } from './app.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Layout } from './layout';
 
 @NgModule({
     declarations: [],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule.withServerTransition({
             appId: 'mam-spa'
         }),

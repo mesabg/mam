21ae76ba08fd5b414e64055f9d89737957a61ef3
Josue Sanchez <josueguitar2@gmail.com>
2018-03-25 00:40:40 -0400

    LC

--- src/app/@components/thumbnails/thumbnail-block-image/thumbnail-block-image.component.ts
+++ src/app/@components/thumbnails/thumbnail-block-image/thumbnail-block-image.component.ts
@@ -23,9 +23,10 @@ declare const $:any;
 })
 export class ThumbnailBlockImageComponent implements OnInit {
 	@Input('images') public images:ImageResponse[];
+	public slideLength;
 	constructor() { }
 	ngOnInit() { 
-		
+		this.slideLength = this.images.length;
 		
 	}
 	public showVelo(index){
@@ -35,5 +36,33 @@ export class ThumbnailBlockImageComponent implements OnInit {
 	public disposeVelo(index){
 		$("body").removeClass("blockScroll");
 		$(`.velo-image[data-index="${index}"]`).fadeOut(600);
+	}
+	public nextSlide(index){
+		this.disposeVeloFast(index);
+		if(index +1 <= this.slideLength -1){
+			this.showVeloFast(index+1);
+		}
+		else {
+			this.showVeloFast(0);
+		}	
+		
 	} 
+	public prevSlide(index){
+		this.disposeVeloFast(index);
+		if(index -1 > 0){
+			this.showVeloFast(index-1);
+		}
+		else {
+			this.showVeloFast(this.slideLength-1);
+		}	
+	}
+	
+	public showVeloFast(index){
+		$("body").addClass("blockScroll");
+		$(`.velo-image[data-index="${index}"]`).show();
+	}
+	public disposeVeloFast(index){
+		$("body").removeClass("blockScroll");
+		$(`.velo-image[data-index="${index}"]`).hide();
+	}
 }
